bd0b0884bc9a344610cbe31cdcbddf510e5dcd28
Mahfuzur Rahman <mahfuzurrahman0012@gmail.com>
2026-03-30 23:58:55 +0600

    update

--- src/Pages/Public/Home/Banner.jsx
+++ src/Pages/Public/Home/Banner.jsx
@@ -1,25 +1,41 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import bannerImg1 from '../../../assets/delivery1.png';
-import bannerImg2 from '../../../assets/delivery2.webp';
-import bannerImg3 from '../../../assets/delivery3.png';
 
 const Banner = () => {
+    const videos = [
+        "https://cdn.dribbble.com/userupload/43815550/file/original-f356998b7c1370a514912f2f3f426e69.mp4",
+        "https://cdn.dribbble.com/userupload/3592456/file/original-600055e7d75ac5c434520de2146147b0.mp4",
+        "https://cdn.dribbble.com/userupload/3717874/file/original-14925efac7bf8695238b8154bf457763.mp4",
+        "https://cdn.dribbble.com/userupload/18331836/file/original-597c38b76bf2b846ed390bee5c9cfed9.mov",
+        "https://cdn.dribbble.com/userupload/43430747/file/original-740b1773eb207c5fd2f354206b0ff61a.mp4"
+    ];
+
     return (
-        <Carousel autoPlay={true} infiniteLoop={true} interval={"1500"} showThumbs={false} showStatus={false}>
-            <div>
-                <img className='h-[500px]' src={bannerImg1} />
-
-            </div>
-            <div>
-                <img className='h-[500px]' src={bannerImg2} />
-
-            </div>
-            <div>
-                <img className='h-[500px]' src={bannerImg3} />
-
-            </div>
+        <Carousel
+            autoPlay={true}
+            infiniteLoop={true}
+            interval={5000}
+            showThumbs={false}
+            showStatus={false}
+            stopOnHover={false}
+            className='mb-10'
+        >
+            {
+                videos.map((video, index) => (
+                    <div key={index}>
+                        <video
+                            className="w-full h-[480px] object-cover"
+                            autoPlay
+                            muted
+                            loop
+                            playsInline
+                        >
+                            <source src={video} type="video/mp4" />
+                        </video>
+                    </div>
+                ))
+            }
         </Carousel>
     );
 };
